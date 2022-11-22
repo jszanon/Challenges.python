7011de8975703a81e9ecadf0b58e2799025e8e18
Jessica Sanzoni Zanon <102073253+jszanon@users.noreply.github.com>
2022-11-22 18:14:51 +0000

    Add files via upload

---
+++ Challenge3.py
@@ -0,0 +1,6 @@
+# Challenge 3: Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+n3 = n1 + n2
+print(f'A soma entre {n1} e {n2} é {n3}.')
