58eb632c9473ff58ed65c8a51f99f3d28db825de
Jessica Sanzoni Zanon <102073253+jszanon@users.noreply.github.com>
2022-11-22 18:16:07 +0000

    Add files via upload

---
+++ Challenge6.py
@@ -0,0 +1,6 @@
+# Challenge 6: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raíz quadrada
+
+n = int(input('Digite um número inteiro: '))
+print(f'O dobro de {n} vale {n*2}.')
+print(f'O triplo de {n} vale {n*3}.')
+print(f'A raíz quadrada de {n} vale {n**(1/2)}')
---
+++ Challenge7.py
@@ -0,0 +1,6 @@
+# Challenge 7: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite  a segunda nota: '))
+media = (n1 + n2) / 2
+print(f'A média das notas {n1:.1f} e {n2:.1f} é {media:.1f}.')
