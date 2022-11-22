b01908eb1f7c91e780c3c89448842a827044d30a
Jessica Sanzoni Zanon <102073253+jszanon@users.noreply.github.com>
2022-11-22 18:15:44 +0000

    Add files via upload

---
+++ Challenge5.py
@@ -0,0 +1,4 @@
+# Challenge 5: Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e antecessor.
+
+num = int(input('Digite um número: '))
+print(f'O antecessor de {num} é {num-1} e seu sucessor é {num+1}.')
